2efa493d6ae6586f604cce2dde33df4bb66732e9
Anton <antongolova@gmail.com>
2019-03-22 11:34:38 +0300

    Логотип

--- src/mixins/pdfAnketa.js
+++ src/mixins/pdfAnketa.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { mapGetters, mapState } from 'vuex';
 import portalApi from '@/services/portalApi';
 
@@ -22,6 +23,19 @@ export default {
 						weight: this.user.weight,
 					})
 					.then(result => {
+						axios({
+							url: `https://medaboutme.ru/zdorove/servisy/dispanserizatsiya/download-result/${result.id}/`,
+							method: 'GET',
+							responseType: 'blob', // important
+						}).then((response) => {
+							const url = window.URL.createObjectURL(new Blob([response.data]));
+							const link = document.createElement('a');
+							link.href = url;
+							link.setAttribute('download', 'file.pdf'); //or any other extension
+							document.body.appendChild(link);
+							link.click();
+						});
+						
 						window.open(
 							`https://medaboutme.ru/zdorove/servisy/dispanserizatsiya/download-result/${
 								result.id
